1d31fdd9f13cb2834f459befaa0d632a95d4826b
Waldemar Schroeer <waldemar.schroeer@de.ttiinc.com>
2022-01-10 14:43:30 +0100

    Add support for colors.

--- prepare-test.sh
+++ prepare-test.sh
@@ -11,6 +11,22 @@
 cdir=$(pwd)
 logfile=prepare.log
 
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
 # +----- Functions ---------------------------------------------------------+
 display_Warning () {
     /bin/clear
@@ -36,7 +52,7 @@ display_Warning () {
 
 get_User () {
     if ! [[ $(id -u) = 0 ]]; then
-        echo "\nError: This script must be run as root.\n\n" 
+        printf "%40s\n" "${RED}Error:${NORMAL} This script must be run as root.\n\n" 
         exit 1
     fi
 }
